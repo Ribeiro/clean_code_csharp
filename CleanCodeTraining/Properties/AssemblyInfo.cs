4d59f07297c1c678b828a4c63c8915d4c0efb01e
Geovanny Ribeiro <geovannyribeiro@solsis.br>
2016-06-15 09:27:49 -0300

    added log4net reference on AssemblyInfo.cs

--- CleanCodeTraining/Properties/AssemblyInfo.cs
+++ CleanCodeTraining/Properties/AssemblyInfo.cs
@@ -22,6 +22,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("47dbb553-b5a7-4e10-a296-2e163b739ecc")]
 
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
